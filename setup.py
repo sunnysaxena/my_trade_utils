45c9387018dbf2c4198185e7822d734d7f1e5591
SUNNY SAXENA <saxenasunny@hotmail.com>
2024-05-17 17:51:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.10'
+    python_requires='>=3.10',
     install_requires=[
-        'pandas',
+        'pandas'
     ]
 )
 
